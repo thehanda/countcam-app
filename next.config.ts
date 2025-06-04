b331721d2b6ade1bb94734ebaacef1b8183d7d1e
Handa The <TheHanda@gmail.com>
2025-06-04 05:03:43 +0000

    おめでとう。Code画面に切り替えたら、webアプリのUI表示になりました。 以下は、分割前のターミナルです。 GET / 200 in 274ms  GET / 200 in 195ms  GET /?monospaceUid=222394 200 in 412ms  ⚠ Blocked cross-origin request from 9000-firebase-studio-1748576929604.cluster-ikxjzjhlifcwuroomfkjrx437g.cloudworkstations.dev. To allow this, configure "allowedDevOrigins" in next.config Read more: https://nextjs.org/docs/app/api-reference/config/next-config-js/allowedDevOrigins  GET / 200 in 353ms  GET / 200 in 229ms  GET / 200 in 487ms  GET / 200 in 295ms  GET / 200 in 166ms Shutting down all Genkit servers...
    studio-1411938260:~/studio{master}$
    studio-1411938260:~/studio{master}$

--- next.config.ts
+++ next.config.ts
@@ -21,11 +21,9 @@ const nextConfig: NextConfig = {
   },
   allowedDevOrigins: [
     'https://6000-firebase-studio-1748576929604.cluster-ikxjzjhlifcwuroomfkjrx437g.cloudworkstations.dev',
+    'http://6000-firebase-studio-1748576929604.cluster-ikxjzjhlifcwuroomfkjrx437g.cloudworkstations.dev',
     'https://9000-firebase-studio-1748576929604.cluster-ikxjzjhlifcwuroomfkjrx437g.cloudworkstations.dev',
-    // You might see other ports or subdomains in the future.
-    // If the random-looking parts of the domain change frequently,
-    // a more generic pattern might be needed if Next.js supports it,
-    // or you'll need to update this list.
+    'http://9000-firebase-studio-1748576929604.cluster-ikxjzjhlifcwuroomfkjrx437g.cloudworkstations.dev',
   ],
   // reactStrictMode: false, // Intentionally commented out or removed to allow default (true)
 };
